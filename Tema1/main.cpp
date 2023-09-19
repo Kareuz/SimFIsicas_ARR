c399dc9a75d654b11440c0e0eeee1b362f732d7b
Alex <112498727+Kareuz@users.noreply.github.com>
2023-09-19 10:34:57 -0600

    Subiendo el Tema 1 de Simuladores de Físicas
    En el tema 1 creamos un cubo, un suelo y le agregamos gravedad al cubo

---
+++ Tema1/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <Box2d/Box2d.h>
+
+using namespace std;
+
+int main() {
+    b2Vec2 gravity(0.0f, -9.81f);
+    b2World world(gravity);
+
+   b2BodyDef groundBodyDef;  //Se crea un cuerpo
+   groundBodyDef.position.Set(0.0f, -10.0f);  //Se define la posición del cuerpo
+
+    b2Body* groundBody = world.CreateBody(&groundBodyDef); //Se llama al cuerpo al mundo
+
+    b2PolygonShape groundBox; //Se crea una forma a un cuerpo
+    groundBox.SetAsBox(50.0f, 10.0f);  //Se establecen las medidas de la forma, en este caso, será un cuadrado
+
+    groundBody->CreateFixture(&groundBox, 0.0f);
+
+    //
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody; //El tipo de cuerpo será dinamico ya que su posición va a variar
+    bodyDef.position.Set(0.0f, 4.0f);
+    b2Body* body = world.CreateBody(&bodyDef);
+
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f, 1.0f);
+
+    b2FixtureDef fixtureDef; //Se define una característica
+    fixtureDef.shape = &dynamicBox; //Se le aplica la característica a la forma
+    fixtureDef.density = 1.0f; //Característica de densidad
+    fixtureDef.friction = 0.3f; //Característica de fricción
+
+    body->CreateFixture(&fixtureDef); //Se le aplican las características anteriores al "body"
+
+    float timeStep = 1.0f / 60.0f; //Determinamos cada cuanto se realiza una acción, interación o lo que sea
+
+    int32 velocityIterations = 6;
+    int32 positionIterations = 2;
+
+    for (int32 i = 0; i < 60; ++i)
+    {
+        world.Step(timeStep, velocityIterations, positionIterations);
+        b2Vec2 position = body->GetPosition();
+        float angle = body->GetAngle();
+        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+    }
+}
